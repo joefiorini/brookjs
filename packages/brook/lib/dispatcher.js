9e764a4c6d7276e8ce4c76ce2b4b542b240c2361
Joe Fiorini <joe@joefiorini.com>
2013-05-03 00:49:48 -0400

    Rename private state variable

--- packages/brook/lib/dispatcher.js
+++ packages/brook/lib/dispatcher.js
@@ -1,28 +1,28 @@
 function init(subscribe){
 
-  var me = {}, subscribers = [];
+  var self = {}, subscribers = [];
 
-  me.push = function(e){
+  self.push = function(e){
     subscribers.forEach(function(s){
       s(e);
     });
   };
 
-  me.subscribe = function(subscriber){
+  self.subscribe = function(subscriber){
     subscribers = subscribers.concat(subscriber);
 
     if(subscribers.length == 1){
       // All subscribers will push to stream when they call
       //  their sink() function; sink is "push".
-      subscribe(me.push);
+      subscribe(self.push);
     }
   };
 
-  me.subscribed = function(test){
+  self.subscribed = function(test){
     return subscribers.indexOf(test) >= 0;
   };
 
-  return Object.create(me);
+  return Object.create(self);
 
 }
 
